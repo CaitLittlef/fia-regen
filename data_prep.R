5a41862e4afebbf0b353766be97e7bccfffac10e
Caitlin Littlefield <clittlef@uw.edu>
2019-01-16 12:51:31 -0500

    excluded non-forested, multiple conditions

--- data_prep.R
+++ data_prep.R
@@ -32,12 +32,8 @@ count(plot, STATECD)
 # 11      53    13 WA
 # 12      56  5381 WY
 
-## Alt: 
-plot %>%
-  group_by(STATECD) %>%
-  summarize(n=n())
 
-
+#####################################
 ## Confirm there are no dupe plots
 plot %>% 
   group_by(CN) %>% 
@@ -45,16 +41,52 @@ plot %>%
   summarize(n=n()) # to see that no, there are no dupes
 
 
+## Drop field types that aren't forested
+# FLDTYPCD assigned by crew; 3-dig code per type
+# Also shouldn't have: 
+# 980 Tropical hardwoods group
+# 988 Cloud forest
+# 990 Exotic hardwoods group
+# 999 Nonstocked
+any(cond.tree.seed$FLDTYPCD>979) # TRUE
+cond.tree.seed %>% 
+  filter(FLDTYPCD > 979) %>%
+  group_by(FLDTYPCD) %>% 
+  summarize(n=n()) # only one record -- delete it.
+# Per manual, 995 = other exotic hardwoods. DELETE.
+cond.tree.seed <- filter(cond.tree.seed, ! is.na(FLDTYPCD) | FLDTYPCD > 979)
+
+
+## Choose plots where all 4 subplots are in same condition
+# Sum of all condition proportions for a plot = 1.
+# If CONDPROP_UNADJ < 1, that condition isn't the only one.
+cond.tree.seed <-filter(cond.tree.seed, CONDPROP_UNADJ==1)
+dim(cond.tree.seed) # 27540
+
+
+#####################################
 ## Assign ID unique to plot AND yr; ID unqiue to plot, that's same across yrs.
 attach(cond.tree.seed)
 cond.tree.seed$UNIQUEID<-paste(PLOT,STATECD,COUNTYCD,INVYR,sep='_')
 cond.tree.seed$PLOTID<-paste(PLOT,STATECD,COUNTYCD,sep='_')
 detach(cond.tree.seed)
-dim(cond.tree.seed) # 125673
+dim(cond.tree.seed) # 27540
 
+
+#####################################
 ## Attach lat/long/elev
 data <- merge(cond.tree.seed,plot[,c(1,10,11,12)], by.x ='PLT_CN',by.y='CN')
 data <- merge(cond.tree.seed,
               plot[,c("CN", "LAT_FS", "LON_FS", "ELEV")],
               by.x ='PLT_CN', by.y='CN')
 plot(data$LON_FS, data$LAT_FS, pch=19, cex=0.1)
+
+
+## Scope revisited plots
+data %>%
+  group_by(PLOTID) %>%
+  mutate(FREQ=n()) %>%
+  filter(FREQ > 1) %>%
+  with(plot(LON_FS, LAT_FS, # with b/c plot doesn't take arg of dataset
+            pch=19, cex=0.1)) 
+# WYO virtually excluded
