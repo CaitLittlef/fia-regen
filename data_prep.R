48c744e585f719e8ffc614ec5390c9a58dceaca0
Caitlin Littlefield <clittlef@uw.edu>
2019-01-16 16:11:20 -0500

    weeded out records w/ inventory yr duplicated (e.g., plot X & INV = 2010 appeared twice.... but seemingly only b/c duff & litter layers were slighly different); generated table of plots and inventory years (improvement over year function); pulled plots that burned and were sampled (latest inventory after most recent fire)

--- data_prep.R
+++ data_prep.R
@@ -13,10 +13,10 @@ mtbs <- read.csv('SP_MTBS_CMBJoin.csv')               ## severity; CMB=climate m
 time.since.fire <- read.csv('SP_TimeSinceFire.csv')   ## time since fire; NIMS=Nat Info Mgmt System
 
 
-## Quick peak: plot lat/long
+## Quick peek
 plot(plot.data$LON_FS, plot.data$LAT_FS, pch=19, cex=0.1)
 # CO, OR, not present; WY "sparse"
-# How many pts each? (lu states from FIADB manual)
+# How many pts each? 
 count(plot, STATECD)
 # STATECD     n
 # 1        4 19867 AZ
@@ -33,7 +33,12 @@ count(plot, STATECD)
 # 12      56  5381 WY
 
 
+
+
 #####################################
+## CONDITION SELETIONS
+#####################################
+
 ## Confirm there are no dupe plots
 plot %>% 
   group_by(CN) %>% 
@@ -64,29 +69,119 @@ cond.tree.seed <-filter(cond.tree.seed, CONDPROP_UNADJ==1)
 dim(cond.tree.seed) # 27540
 
 
+
+
 #####################################
+## UNIQUE IDS FOR PLOTS
+#####################################
+
 ## Assign ID unique to plot AND yr; ID unqiue to plot, that's same across yrs.
 attach(cond.tree.seed)
 cond.tree.seed$UNIQUEID<-paste(PLOT,STATECD,COUNTYCD,INVYR,sep='_')
 cond.tree.seed$PLOTID<-paste(PLOT,STATECD,COUNTYCD,sep='_')
 detach(cond.tree.seed)
+# Put those IDs up front for ease of access
+cond.tree.seed <- cond.tree.seed %>%
+  select(PLOTID, UNIQUEID, everything())
 dim(cond.tree.seed) # 27540
 
-
-#####################################
 ## Attach lat/long/elev
-data <- merge(cond.tree.seed,plot[,c(1,10,11,12)], by.x ='PLT_CN',by.y='CN')
 data <- merge(cond.tree.seed,
               plot[,c("CN", "LAT_FS", "LON_FS", "ELEV")],
               by.x ='PLT_CN', by.y='CN')
 plot(data$LON_FS, data$LAT_FS, pch=19, cex=0.1)
 
 
-## Scope revisited plots
-data %>%
+
+
+
+#####################################
+## REVISITED PLOTS
+#####################################
+
+## Multiple records per PLOTID -- 1 each inventory yr
+moo <- data %>%
   group_by(PLOTID) %>%
   mutate(FREQ=n()) %>%
   filter(FREQ > 1) %>%
-  with(plot(LON_FS, LAT_FS, # with b/c plot doesn't take arg of dataset
-            pch=19, cex=0.1)) 
-# WYO virtually excluded
+  select(PLOTID, FREQ, INVYR) %>%
+  as.data.frame()
+max(moo$FREQ) # 4 is max time a plot visited
+
+# But quick looksee shows some plots have dupe yrs...
+# Only litter/duff depth seem diff in same-yr records.
+# moo %>%
+#   group_by(PLOTID, INVYR) %>%
+#   summarise(DBL.YR =n()) %>% as.data.frame() # yup, some doubles
+
+# Keep only one distinct record.
+moo <- distinct(moo)
+# moo %>%
+#   group_by(PLOTID, INVYR) %>%
+#   summarise(DBL.YR =n()) %>% as.data.frame() # looks like only 1s...
+
+# Re-run frequency
+doo <- moo %>%
+  select(-FREQ) %>%
+  group_by(PLOTID) %>%
+  mutate(FREQ=n()) %>%
+  as.data.frame()
+max(doo$FREQ) # 3 is max time a plot visited
+
+# Populate table with years visited
+plots.rev <- doo %>%
+  group_by(PLOTID) %>%
+  mutate(YR.1ST = nth(INVYR, 1), # select 1st instance
+         YR.2ND = nth(INVYR, 2), # select 2nd instance
+         YR.3RD = nth(INVYR, 3)) %>% # select 3rd instance
+  select(-INVYR) %>%
+  distinct() # needed b/c plots have same # records as visits
+dim(plots.rev) # 5892
+
+# Clean-up
+rm(moo, doo)
+
+
+
+
+
+
+#####################################
+## MTBS
+#####################################
+## Generate unique ID for mtbs plots
+mtbs$PLOTID<-paste(mtbs$PLOT,mtbs$STATECD,mtbs$COUNTYCD,sep='_')
+mtbs <- mtbs %>% select(PLOTID, everything())
+
+## Which of the plots burned?
+data.burned <- inner_join(data, mtbs, by = "PLOTID") #3536 plots
+data.burned <- rename(data.burned, FIRE.YR = Year)
+
+
+## How many were sampled after burned (one or more times, one or more burns)?
+data.burned %>%
+  filter(INVYR > FIRE.YR) %>%
+  count(PLOTID) # 1992 plots sampled post-burn (incl. some multiple samples)
+
+
+## How many were sampled twice after burn?
+# Recall group_by un-group for final delivery so have to count()
+data.burned %>%
+  filter(INVYR > FIRE.YR) %>%
+  group_by(PLOTID) %>%
+  mutate(FREQ.BURN.SAMP = n()) %>%
+  filter(FREQ.BURN.SAMP > 1) %>%
+  count(PLOTID) # 442 plots were sampled multiple times post-burn
+
+
+## List plots that burned and were sampled after -- keep latest visit
+data.burned.samp <- data.burned %>%
+  filter(INVYR > FIRE.YR) %>%
+  group_by(PLOTID) %>%
+  mutate(LATEST.INVYR = max(INVYR)) %>%
+  mutate(LATEST.FIRE = max(FIRE.YR)) %>% # Nix reburns (saw at least 1)
+  filter(LATEST.FIRE > 0) %>% # at 1992 (matches above) til X these zeros
+  select(PLOTID, LATEST.INVYR, LATEST.FIRE) %>%
+  distinct() # 1971 records of latest inventory post-most recent burn
+
+
