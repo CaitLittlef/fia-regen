0020e3b1b577adf22f8ee4aee7d46b6554f32938
Caitlin Littlefield <clittlef@uw.edu>
2019-02-04 15:02:27 -0500

    changed package requirements

--- 00_setup.R
+++ 00_setup.R
@@ -7,14 +7,14 @@
 # wd <- setwd("//goshawk.sefs.uw.edu/Space_Lawler/Shared/BackedUp/Caitlin/RMRS")
 # wd <- setwd("D:/Shared/BackedUp/Caitlin/RMRS") # If working from within goshawk
 wd <- setwd("C:/Users/clittlef/Google Drive/2RMRS/fia-regen/data") # If working with/within drive
-
+out.dir <- "C:/Users/clittlef/Google Drive/2RMRS/fia-regen/output/"
 
 
 #####################################
 # Install packages if not already installed
-required.packages <- c("ggplot2", "raster", "sp", "rgdal", "dplyr",
+required.packages <- c("ggplot2", "raster", "sf", "rgdal", "dplyr",
                        "tidyverse", "maptools", "rgeos", 
-                       "party", "vcd", "maps", "mgcv", "tmap")
+                       "partykit", "vcd", "maps", "mgcv", "tmap")
 new.packages <- required.packages[!(required.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)>0) install.packages(new.packages)
 rm(required.packages, new.packages)
@@ -22,13 +22,14 @@ rm(required.packages, new.packages)
 # Libraries
 library(ggplot2)
 library(raster)
-library(sp)
+# library(sp)
+library(sf)
 library(rgdal)
 library(dplyr)
 library(tidyverse)
 library(maptools)
 library(rgeos)
-library(party)
+library(partykit)
 library(vcd)
 library(maps)
 library(mgcv)
