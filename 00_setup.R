9b9d7d5a5af9457e0445f968c5e5a638f3e13b02
Caitlin Littlefield <clittlef@uw.edu>
2019-02-26 17:33:17 -0500

    added functions (CV, Morans I, multiplot)

--- 00_setup.R
+++ 00_setup.R
@@ -16,7 +16,7 @@ required.packages <- c("ggplot2", "raster", "sf", "rgdal", "dplyr",
                        "tidyverse", "maptools", "rgeos", 
                        "partykit", "vcd", "maps", "mgcv", "tmap",
                        "MASS", "pROC", "ResourceSelection", "caret", "boot",
-                       "dismo")
+                       "dismo", "pscl")
 new.packages <- required.packages[!(required.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)>0) install.packages(new.packages)
 rm(required.packages, new.packages)
@@ -42,6 +42,7 @@ library(ResourceSelection)
 library(caret)
 library(boot)
 library(dismo)
+library(pscl)
 
 # rm(GCtorture)
 
@@ -65,4 +66,69 @@ Wsts <- NAmer[NAmer$NAME %in% sts, ] # not NAmer@data$NAME
 plot(Wsts) # Get multiple b/c multiple attributes
 plot(st_geometry(Wsts)) # Just outline
 crs(Wsts) # "+proj=longlat +datum=NAD83 +no_defs"
+
+
+#####################################
+# Functions
+
+### COEFFICIENT OF VARIATION
+# CV <- function(x) {100*sd(x) / mean(x)}
+
+
+### MORANS I FUNCTION
+Moran_tpha <-function(x)
+{cbind(x$long, x$lat) %>%
+    dist(.) %>%
+    as.matrix(.) %>%
+    .^(-1) -> temp
+  diag(temp) <- 0
+  Moran.I(x$tpha, temp)}
+
+
+Moran_space <-function(x, y)
+{cbind(x$long, x$lat) %>%
+    dist(.) %>%
+    as.matrix(.) %>%
+    .^(-1) -> temp
+  diag(temp) <- 0
+  Moran.I(y, temp)}
+
+
+### Multiplot function
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
 
