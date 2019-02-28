4531f347a72241936abcf20987b0585efcd58efa
Caitlin Littlefield <clittlef@uw.edu>
2019-02-28 12:55:51 -0500

    dumping count models (poisson & neg bin) at bottom

--- 04_spp_models_glm_cnt.R
+++ 04_spp_models_glm_cnt.R
@@ -1,40 +1,35 @@
+## How do regen counts compare between burned plots and unburned plots?
+
 currentDate <- Sys.Date()
 
-
 ## Load data; remove extraneous column if necessary
-data.pied <- read.csv("data.pied_wwoburn.csv") ; data.pied$X <- NULL
-data.pipo <- read.csv("data.pipo_wwoburn.csv") ; data.pipo$X <- NULL
-data.psme <- read.csv("data.psme_wwoburn.csv") ; data.psme$X <- NULL
+data.pipo <- read.csv("data.pipo.csv") ; data.pipo$X <- NULL
+identical(data.pipo$FIRE.YR.x, data.pipo$FIRE.YR.y)
+data.pipo$FIRE.YR <- data.pipo$FIRE.YR.x
+data.pipo$FIRE.YR.x <- NULL ; data.pipo$FIRE.YR.y <- NULL
+
+data.psme <- read.csv("data.psme.csv") ; data.psme$X <- NULL
+identical(data.psme$FIRE.YR.x, data.psme$FIRE.YR.y)
+data.psme$FIRE.YR <- data.psme$FIRE.YR.x
+data.psme$FIRE.YR.x <- NULL ; data.psme$FIRE.YR.y <- NULL
 
 ## Exclude sites w/o fire OR w/ fire.sev 5 & 6 (here NA)
-# data.pied <- data.pied[! is.na(data.pied$FIRE.SEV) ,]
 # data.pipo <- data.pipo[! is.na(data.pipo$FIRE.SEV) ,]
 # data.psme <- data.psme[! is.na(data.psme$FIRE.SEV) ,]
 
-## Set variable classes
-# data.pied$regen_pied <- factor(data.pied$regen_pied, ordered = FALSE)
+## Correct class of variables
 # data.pipo$regen_pipo <- factor(data.pipo$regen_pipo, ordered = FALSE)
+data.pipo$regen_pipo <- as.numeric(as.character(data.pipo$regen_pipo)) 
 # data.psme$regen_psme <- factor(data.psme$regen_psme, ordered = FALSE)
-data.pied$regen_pied <- as.numeric(as.character(data.pied$regen_pied)) # else gives factor level position
-data.pipo$regen_pipo <- as.numeric(as.character(data.pipo$regen_pipo)) # else gives factor level position
-data.psme$regen_psme <- as.numeric(as.character(data.psme$regen_psme)) # else gives factor level position
-data.pied$regen_pied_tpa <- as.integer(round(data.pied$regen_pied_tpa,0))
-data.pipo$regen_pipo_tpa <- as.integer(round(data.pipo$regen_pipo_tpa,0))
-data.psme$regen_psme_tpa <- as.integer(round(data.psme$regen_psme_tpa,0))
-# data.pied$FIRE.SEV <- factor(data.pied$FIRE.SEV, ordered = TRUE)
+data.psme$regen_psme <- as.numeric(as.character(data.psme$regen_psme)) 
 # data.pipo$FIRE.SEV <- factor(data.pipo$FIRE.SEV, ordered = TRUE)
 # data.psme$FIRE.SEV <- factor(data.psme$FIRE.SEV, ordered = TRUE)
-data.pied$FIRE.SEV <- as.numeric(data.pied$FIRE.SEV) # else generates diff var for each factor level
-data.pipo$FIRE.SEV <- as.numeric(data.pipo$FIRE.SEV) # else generates diff var for each factor level
-data.psme$FIRE.SEV <- as.numeric(data.psme$FIRE.SEV) # else generates diff var for each factor level
-data.pied$REBURN <- factor(data.pied$REBURN, ordered = TRUE)
-data.pipo$REBURN <- factor(data.pipo$REBURN, ordered = TRUE)
-data.psme$REBURN <- factor(data.psme$REBURN, ordered = TRUE)
+data.pipo$FIRE.SEV <- as.numeric(data.pipo$FIRE.SEV)
+data.psme$FIRE.SEV <- as.numeric(data.psme$FIRE.SEV)
 
-# Create CMD relative chng
-data.pied$CMD_CHNG <- (data.pied$CMD_2025 - data.pied$CMD_1995)/data.pied$CMD_1995
-data.pipo$CMD_CHNG <- (data.pipo$CMD_2025 - data.pipo$CMD_1995)/data.pipo$CMD_1995
-data.psme$CMD_CHNG <- (data.psme$CMD_2025 - data.psme$CMD_1995)/data.psme$CMD_1995
+## Create CMD_CHNG from observed trend in z-scores
+data.pipo$CMD_CHNG <- data.pipo$def59.z.slope
+data.psme$CMD_CHNG <- data.psme$def59.z.slope
 
 
 ## Set bins for year.diff so include unburned (mtbs only back til 1984)
@@ -49,29 +44,6 @@ data.pipo$YEAR.DIFF.BIN <- as.numeric(data.pipo$YEAR.DIFF.BIN) # if modelling
 # data.pipo$YEAR.DIFF.BIN <- factor(data.pipo$YEAR.DIFF.BIN, ordered = TRUE) # if plotting
 
 
-## glm of pipo regen; 
-mean(data.pipo$regen_pipo_tpa) ; sd(data.pipo$regen_pipo_tpa)
-# ^ suggests overdisprsion. Try poissoin & negative binomial
-# Poisson
-mod.pipo.cnt.ps <- glm(regen_pipo_tpa ~ YEAR.DIFF.BIN
-                       + BALive_pipo
-                       + MAP_1995 
-                       + CMD_CHNG
-                       + FIRE.SEV,
-                       data = data.pipo, poisson(link = "log"))
-summary(mod.pipo.cnt.ps)
-
-# Negative binomial
-mod.pipo.cnt.nb <- glm.nb(regen_pipo_tpa ~ YEAR.DIFF.BIN
-                          + BALive_pipo
-                          + MAP_1995 
-                          + CMD_CHNG,
-                          + FIRE.SEV,
-                          data = data.pipo, link = "log")
-summary(mod.pipo.cnt.nb)
-pchisq(2 * (logLik(mod.pipo.cnt.nb) - logLik(mod.pipo.cnt.ps)), df = 1, lower.tail = FALSE)
-# Negative binomial models assume the conditional means are not equal to the conditional variances. This inequality is captured by estimating a dispersion parameter (not shown in the output) that is held constant in a Poisson model. Thus, the Poisson model is actually nested in the negative binomial model. We can then use a likelihood ratio test to compare these two and test this model assumption.
-# Here, reject null hypothesis that additional parameter theta (dispersion) does NOT improve fit
 
 
 ## log won't take 0 so set slightly above 
@@ -164,3 +136,30 @@ p
 #      width = 640, height = 480, units = "px")
 # p
 # dev.off()
+
+
+################################################################## counts with negative binomial
+## glm of pipo regen; 
+# mean(data.pipo$regen_pipo_tpa) ; sd(data.pipo$regen_pipo_tpa)
+# # ^ suggests overdisprsion. Try poissoin & negative binomial
+# # Poisson
+# mod.pipo.cnt.ps <- glm(regen_pipo_tpa ~ YEAR.DIFF.BIN
+#                        + BALive_pipo
+#                        + MAP_1995 
+#                        + CMD_CHNG
+#                        + FIRE.SEV,
+#                        data = data.pipo, poisson(link = "log"))
+# summary(mod.pipo.cnt.ps)
+# 
+# # Negative binomial
+# mod.pipo.cnt.nb <- glm.nb(regen_pipo_tpa ~ YEAR.DIFF.BIN
+#                           + BALive_pipo
+#                           + MAP_1995 
+#                           + CMD_CHNG,
+#                           + FIRE.SEV,
+#                           data = data.pipo, link = "log")
+# summary(mod.pipo.cnt.nb)
+# pchisq(2 * (logLik(mod.pipo.cnt.nb) - logLik(mod.pipo.cnt.ps)), df = 1, lower.tail = FALSE)
+# # Negative binomial models assume the conditional means are not equal to the conditional variances. This inequality is captured by estimating a dispersion parameter (not shown in the output) that is held constant in a Poisson model. Thus, the Poisson model is actually nested in the negative binomial model. We can then use a likelihood ratio test to compare these two and test this model assumption.
+# # Here, reject null hypothesis that additional parameter theta (dispersion) does NOT improve fit
+
