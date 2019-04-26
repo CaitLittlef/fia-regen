f76b78859629f02befe3d2889da13be21914a82c
Caitlin Littlefield <clittlef@uw.edu>
2019-04-26 14:31:54 -0400

    add multiple deficits sequences; update data to load

--- 04_spp_models_partykit.R
+++ 04_spp_models_partykit.R
@@ -5,8 +5,8 @@
 currentDate <- Sys.Date()
 
 ## Load data; remove extraneous column if necessary
-data.pipo <- read.csv("data.pipo.csv") ; data.pipo$X <- NULL
-data.psme <- read.csv("data.psme.csv") ; data.psme$X <- NULL
+data.pipo <- read.csv("data.pipo_2019-04-24.csv") ; data.pipo$X <- NULL
+data.psme <- read.csv("data.psme_2019-04-24.csv") ; data.psme$X <- NULL
 
 ## Exclude sites w/o fire OR w/ fire.sev 5 & 6 (here NA)
 data.pipo <- data.pipo[! is.na(data.pipo$FIRE.SEV) ,]
@@ -43,27 +43,44 @@ data.psme$BAProp_psme <- data.psme$BALive_psme/data.psme$BALiveTot
 # May need I(YEAR.DIFF) to treat var "as-is" if using covariate twice.
 # Ref: https://stats.stackexchange.com/questions/390014/default-plot-for-mob-object-glm-tree-not-returned-using-party-package
 # minsplit is f(number obs * weight). Here, w/ weights = 1, equals min number obs in node. 
+# Tree growth is based on statistical stopping rules, so pruning not required (?).
 
 ######################################################
 ### PIPO GLM TREE
 ######################################################
 ## Grow tree
 glm.tree.pipo <- glmtree(regen_pipo ~ YEAR.DIFF |
-                  BALive_pipo +
+                  BALive_pipo + BALiveTot
                 + def.tc
                 + tmax.tc
                 + ppt.tc
-                + CMD_CHNG
-                + def59_z_13
-                # + ELEV # effectively taken care of with climatologies
-                + REBURN
+                + CMD_CHNG 
+                # + def59_z_1
+                # + def59_z_12
+                # + def59_z_13
+                # + def59_z_14
+                # + def59_z_15
+                # + ELEV
+                + REBURN 
                 + FIRE.SEV
                 ,
-                data = data.pipo,
+                data = data.pipo,#[data.pipo$YEAR.DIFF > 3,],
+                # data = data.pipo[data.pipo$BALive_pipo > 0,],
+                # data = data.pipo[data.pipo$TRTCD1 == 0,],
+                # data = data,
                 family = binomial(link = "logit"),
                 minsplit = 50, 
                 ordinal = "L2") 
 plot(glm.tree.pipo)
+
+# z15 shows up when included, 
+# but that makes little sense as many sites weren't sampled > 1 yr post-fire
+data.pipo %>% count(def59_z_15 < -0.032, YEAR.DIFF <5)
+hist(data.pipo$YEAR.DIFF[data.pipo$def59_z_15 < -0.032], 30)
+# tiff(paste0(out.dir, "pipo_yr_diff_low_def_",currentDate,".tiff"),
+#      width = 640, height = 480, units = "px")
+# hist(data.pipo$YEAR.DIFF[data.pipo$def59_z_15 < -0.032], 30)
+# dev.off()
 
 ## Output
 plot(glm.tree.pipo) ; glm.tree.pipo # plot w/ terminal_panel = NULL shows stats
@@ -79,8 +96,6 @@ plot(glm.tree.pipo) ; glm.tree.pipo # plot w/ terminal_panel = NULL shows stats
 ### PIPO GLM TREE EVAL
 ######################################################
 
-## Also see _CV.R for alt ways: cv.glm, MSE, confusion matrices (w/ caret)
-
 ###########
 ## Proportional deviance explained (pseudo R^2). Ref: Zuur et al. 2009 p. 218
 # Create null with just intercept; can't pull null deviance from glm tree only resid dev.
@@ -91,15 +106,20 @@ mod.pipo.null <- glm(regen_pipo ~ 1, data = data.pipo, family = binomial)
 ## C.f., non tree (which ultimately means multiple models vs. 1)
 # Non-tree full
 mod.pipo = glm(regen_pipo ~ YEAR.DIFF
-               + BALive_pipo 
+               + BALive_pipo + BALiveTot
                + def.tc
                + tmax.tc
                + ppt.tc
-               + CMD_CHNG
-               + def59_z_13
-               # + ELEV # effectively taken care of with climatologies
-               + REBURN
-               + FIRE.SEV,
+               + CMD_CHNG 
+               # + def59_z_1
+               # + def59_z_12
+               # + def59_z_13
+               # + def59_z_14
+               # + def59_z_15
+               # + ELEV
+               + REBURN 
+               + FIRE.SEV
+               ,
                data = data.pipo, family = binomial)
 summary(mod.pipo)
 # Non-tree only signif
@@ -110,7 +130,7 @@ mod.pipo.step <- stepAIC(mod.pipo)
 summary(mod.pipo.step) # Suggests dropping FIRE.SEV
 
 # With likelihood ratio test (ok b/c they're nested), which is better? 
-lrtest(mod.pipo.step, mod.pipo.select) # No diff (p > 0.05) so keep more parsimonious step.
+lrtest(mod.pipo.step, mod.pipo.select) # Diff (p > 0.05) so keep select.
 
 
 ###########
@@ -156,198 +176,119 @@ AIC(glm.tree.pipo, mod.pipo.step, mod.pipo.select)
 
 
 
-
-
-########################################## PSME
-## Grow tree
-data.psme$FIRE.SEV <- factor(data.psme$FIRE.SEV, ordered = TRUE)
-glm.tree.psme <- glmtree(regen_psme ~ YEAR.DIFF
-                         | BALive_psme 
+######################################################
+### PSME GLM TREE
+######################################################
+glm.tree.psme <- glmtree(regen_psme ~ YEAR.DIFF 
+                         | BALive_psme + BALiveTot
                          + def.tc
                          + tmax.tc
                          + ppt.tc
-                         + CMD_CHNG
+                         + CMD_CHNG 
                          # + def59_z_1
-                         # + def59_z_2
-                         # + def59_z_3
-                         + def59_z_13
-                         # + ELEV # effectively taken care of with climatologies
-                         + REBURN
+                         # + def59_z_12
+                         # + def59_z_13
+                         # + def59_z_14
+                         # + def59_z_15
+                         # + ELEV
+                         + REBURN 
                          + FIRE.SEV
                          ,
-                         data = data.psme, 
-                         family = binomial(link = "logit"),
-                         minsplit = 100, # weights required for splits, not just number obs
-                         ordinal = "L2") # Use dedicated ordinal stat for ordinal vars (FIRE.SEV)
-
+                         data = data.psme,
+                         # data = data.psme[data.psme$TRTCD1 == 0 ,],
+                         family = binomial,
+                         minsplit = 40, 
+                         ordinal = "L2") 
 
 ## Output
 plot(glm.tree.psme) ; glm.tree.psme
-# Save plot
+  # Save plot
 # tiff(paste0(out.dir,"psme_tree_",currentDate,".tiff"),
 #      width = 640, height = 480, units = "px")
 # plot(glm.tree.psme)
 # dev.off()
 
 
-##########################################################
-## Model eval
-data.psme$regen_psme <- as.numeric(as.character(data.psme$regen_psme)) # need numeric for c.f. pred
 
-## Calculate deviance explained
-# Non-tree null
-mod.psme.null <- glm(regen_psme ~ 1, data = data.psme, family = binomial(link = "logit"))
-# Non-tree year diff
-mod.psme.yrdiff <- glm(regen_psme ~ YEAR.DIFF, data = data.psme, family = binomial(link = "logit"))
-data.psme$FIRE.SEV <- as.numeric(data.psme$FIRE.SEV) # set for simple glm
-data.psme$regen_psme <- as.numeric(as.character(data.psme$regen_psme)) # set for simple glm
+######################################################
+### PSME GLM TREE EVAL
+######################################################
+
+###########
+## Proportional deviance explained (pseudo R^2). Ref: Zuur et al. 2009 p. 218
+# Create null with just intercept; can't pull null deviance from glm tree only resid dev.
+mod.psme.null <- glm(regen_psme ~ 1, data = data.psme, family = binomial)
+# Tree model over non-tree null.
+(deviance(mod.psme.null) - deviance(glm.tree.psme))/deviance(mod.psme.null)
+
+## C.f., non tree (which ultimately means multiple models vs. 1)
 # Non-tree full
 mod.psme = glm(regen_psme ~ YEAR.DIFF
-               + BALive_psme #+ BALiveTot
+               + BALive_psme + BALiveTot
                + def.tc
                + tmax.tc
                + ppt.tc
-               + CMD_CHNG
-               + def59_z_13
-               # + ELEV # effectively taken care of with climatologies
-               + REBURN
-               + FIRE.SEV,
+               + CMD_CHNG 
+               # + def59_z_1
+               # + def59_z_12
+               # + def59_z_13
+               # + def59_z_14
+               # + def59_z_15
+               # + ELEV
+               + REBURN 
+               + FIRE.SEV
+               ,
                data = data.psme, family = binomial)
+summary(mod.psme)
+# Non-tree only signif
+mod.psme.select <- update(mod.psme, regen_psme ~ YEAR.DIFF + BALive_psme + def.tc + ppt.tc + FIRE.SEV)
+summary(mod.psme.select)
 # Non-tree step
 mod.psme.step <- stepAIC(mod.psme)
-summary(mod.psme.step)
-# Non-tree only signif
-# ^ same as step
+summary(mod.psme.step) # Suggests retaining precip, too. Identical.
 
-data.psme$FIRE.SEV <- factor(data.psme$FIRE.SEV, ordered = TRUE) # set back for glm tree
-data.psme$regen_psme <- factor(data.psme$regen_psme, ordered = FALSE) # set back for glm tree
-summary(mod.psme)
-# Tree model over non-tree null.
-(deviance(mod.psme.null) - deviance(glm.tree.psme))/deviance(mod.psme.null)
-# Tree model over non-tree simple.
-(deviance(mod.psme.yrdiff) - deviance(glm.tree.psme))/deviance(mod.psme.yrdiff)
-# Tree model over non-tree full.
-(deviance(mod.psme) - deviance(glm.tree.psme))/deviance(mod.psme)
-# Tree model over non-tree step
-(deviance(mod.psme.step) - deviance(glm.tree.psme))/deviance(mod.psme.step)
-summary(mod.psme.step)
-AIC(glm.tree.psme, mod.psme.null, mod.psme.yrdiff, mod.psme, mod.psme.step)
-
-
-##########
-## MSE
-data.psme$regen_psme <- as.numeric(as.character(data.psme$regen_psme))
-regen_pred <- predict(glm.tree.psme, newdata = data.psme, type ="response")
-(MSE = mean((data.psme$regen_psme - regen_pred)^2))
-
-###########
-## ROC assesses discrimination w/ sensitivity (true pos) & specificity (true neg); want =1
-regen_pred=predict(glm.tree.psme, newdata = data.psme, type ="response")
-data.psme$regen_pred <- regen_pred
-roccurve <- roc(regen_psme ~ regen_pred, data = data.psme)
-par(mfrow=c(1,1))
-plot(roccurve)
-auc(roccurve)
+# With likelihood ratio test (ok b/c they're nested), which is better? 
+lrtest(mod.psme.step, mod.psme.select) # Identical
 
 
 ###########
-## Prediction rate on orig data (confusion matrix) 
-regen_pred <- predict(glm.tree.psme, newdata = data.psme, type = "response") # gives decimals
-thresh <- 0.5 # define threshold for categorizing predicted probabilities.
-# Then cut predicted probabilities into categories 
-predCat <- cut(regen_pred, breaks=c(-Inf, thresh, Inf), labels=c("0", "1"))
-# Create confusion matrix
-confusn <- table(predCat, data.psme$regen_psme, dnn=c("predicted", "actual"))
-addmargins(confusn)
-
-
-## Alt use caret package
-regen_pred <- predict(glm.tree.psme, newdata = data.psme, type = "response") # gives decimals in probability
-confusionMatrix(data = as.factor(as.numeric(regen_pred>0.5)), # needs factor but divide decimals into 0/1 first
-                reference = as.factor(data.psme$regen_psme), # needs factor
-                dnn = c("predicted", "actual"))
-
-
-##
-## Alt self-created hold-out (should ~ accuracy from k-folds cross-validation)
-# Randomly shuffle data
-data<-data.psme[sample(nrow(data.psme)),]
-# Create 10 equally size folds
-folds <- cut(seq(1,nrow(data)),breaks=10,labels=FALSE)
-# Subset test and training data 
-for(i in 1:10){
-  #Segement data by fold 
-  testIndexes <- which(folds==i,arr.ind=TRUE)
-  testData <- data[testIndexes, ]
-  trainData <- data[-testIndexes, ]
-}
-# Train model 
-glm.tree.psme.train <- glmtree(regen_psme ~ YEAR.DIFF
-                               | BALive_psme +  MAP_1995 
-                               + CMD_1995
-                               + CMD_CHNG
-                               + FIRE.SEV,
-                               data = trainData, 
-                               family = binomial(link = "logit"),
-                               minsplit = 50, # weights required for splits, not just number obs
-                               ordinal = "L2") # Use dedicated ordinal stat for ordinal vars (FIRE.SEV)
-
-# Use trained model to predict testing data
-predTest <- predict(glm.tree.psme.train, testData, type = "response")
-thresh <- 0.5 # define threshold for categorizing predicted probabilities.
-# Then cut predicted probabilities into categories 
-predCat <- cut(predTest, breaks=c(-Inf, thresh, Inf), labels=c("0", "1"))
-# Create confusion matrix
-confusn <- table(predCat, testData$regen_psme, dnn=c("predicted", "actual"))
-addmargins(confusn)
-
-## Alt, use caret package
-predTest <- predict(glm.tree.psme.train, testData)
-confusionMatrix(data = as.factor(as.numeric(predTest>0.5)), # needs factor but set num for 0/1 first
-                reference = as.factor(testData$regen_psme), # needs factor
-                dnn = c("predicted", "actual"))
+## AIC
+AIC(glm.tree.psme, mod.psme.step, mod.psme.select)
 
 
 ###########
-## K-fold cross-validation prediction error -- NA for glm trees
+## 10-fold accuracy (from _CV.R script).
+# Should ~ caret accuracy; for non-trees, use cv.glm
+# tree: 0.7110463 
+# non-tree: 0.728169
 
 
-##############################################
-## Grab & plot observations in given node
-## Get obs for each node (orig as df) and save row.names (match orig dataset)
-# Create new col in df to assign node number
-data.psme$NODE <- NA 
-data.psme$NODE %>% factor()
-
-# How many and what ID? Keep only those for assigning. length() gives all, width() gives terminal
-diff <- (length(glm.tree.psme) - width(glm.tree.psme)) # Gives nodes that SHOULDN'T get assigned
-loop.ready <- ((1+diff):(width(glm.tree.psme)+diff)) # gives node value that should get assigned
-
-# Loop through all terminal nodes
-for (i in loop.ready){ # length(summary) to only get terminal nodes
-  node <- paste0("node",(i))
-  obs <- glm.tree.psme[[i]]$data %>% row.names()
-  data.psme$NODE[rownames(data.psme) %in% obs] <- paste0(node)
-}
-
-data.psme %>% group_by(NODE) %>% count()
-
-p <- ggplot() +
-  # state outlines
-  geom_sf(data = Wsts) +
-  geom_point(data = data.psme, aes(x = LON_FS, y = LAT_FS, col = NODE),
-                                   size = 4, alpha = 0.7) +
-  scale_color_manual(values = c("yellow", "orange", "light green", "dark green"),
-                     labels = c("3: <BA <CMD", "4: <BA >CMD", "6: >BA <CMD", "7: >BA >CMD"))
-p
-# tiff(paste0(out.dir,"psme_tree_map_",currentDate,".tiff"),
-#       width = 640, height = 480, units = "px")
-# p
-# dev.off()
+###########
+## 10-fold AUC (from _CV.R script)
+# tree: 0.7949143
+# non-tree: 0.7681333
 
 
 
-
-
-########################################## PIED
-# too few obs
+# ###########
+# ## MSE
+# data.psme$regen_psme <- as.numeric(as.character(data.psme$regen_psme))
+# regen_pred <- predict(glm.tree.psme, newdata = data.psme, type ="response")
+# (MSE = mean((data.psme$regen_psme - regen_pred)^2))
+# 
+# 
+# ###########
+# ## Confusion matrices (but cross-validation better)
+# regen_pred <- predict(glm.tree.psme, newdata = data.psme, type = "response") # gives decimals
+# thresh <- 0.25 # define threshold for categorizing predicted probabilities. Rule of thumb: prevalence of 1s
+# # Then cut predicted probabilities into categories 
+# predCat <- cut(regen_pred, breaks=c(-Inf, thresh, Inf), labels=c("0", "1"))
+# # Create confusion matrix
+# confusn <- table(predCat, data.psme$regen_psme, dnn=c("predicted", "actual"))
+# addmargins(confusn)
+# 
+# ## Alt use caret package
+# regen_pred <- predict(glm.tree.psme, newdata = data.psme, type = "response") # gives decimals in probability
+# confusionMatrix(data = as.factor(as.numeric(regen_pred>0.25)), # needs factor but divide decimals into 0/1 first
+#                 reference = as.factor(data.psme$regen_psme), # needs factor
+#                 dnn = c("predicted", "actual"))
