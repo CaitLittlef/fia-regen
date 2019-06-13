8d2e11e2332e5c413db923db0eab0dac6473fc1b
Caitlin Littlefield <clittlef@uw.edu>
2019-06-12 20:51:39 -0400

    create rasters for prediction; extract predcited values and plot into ggplot

--- 04_spp_models_brt_maps.R
+++ 04_spp_models_brt_maps.R
@@ -17,36 +17,71 @@ def59_z_max15[! is.na(def59_z_max15)] <- mean(data.brt$def59_z_max15)
 DUFF_DEPTH_cm <- r
 DUFF_DEPTH_cm[! is.na(DUFF_DEPTH_cm)] <- mean(data.brt$DUFF_DEPTH_cm)
 
+# Yet the underlying names are still remnants of orig raster file
+labels(tmax.tc) # gives "tmax.1981.2010"
+names(tmax.tc) <- "tmax.tc"
+labels(tmax.tc) # gives "tmax.tc"
+names(YEAR.DIFF) <- "YEAR.DIFF"
+names(BALive_brt_m) <- "BALive_brt_m"
+names(def59_z_max15) <- "def59_z_max15"
+names(DUFF_DEPTH_cm) <- "DUFF_DEPTH_cm"
+
+# Load them all into a brick for predictions
 brick <- brick(YEAR.DIFF, BALive_brt_m, tmax.tc, def59_z_max15, DUFF_DEPTH_cm)
+names(brick)
 
+# Predict!
 space <- raster::predict(brick, models[[1]], n.trees=models[[1]]$n.trees, type = "response")
-
-> p <- predict(Anguilla_grids, angaus.tc5.lr005, const=add,
-               + n.trees=angaus.tc5.lr005$gbm.call$best.trees, type="response")
-brick[[1]]
+plot(space)
 
 
+## To plot raster in ggplot, extract values into tibble
+# ref: https://stackoverflow.com/questions/47116217/overlay-raster-layer-on-map-in-ggplot2-in-r
 
+# Define function to extract raster values into a tibble
+gplot_data <- function(x, maxpixels = 50000)  {
+  x <- raster::sampleRegular(x, maxpixels, asRaster = TRUE)
+  coords <- raster::xyFromCell(x, seq_len(raster::ncell(x)))
+  ## Extract values
+  dat <- utils::stack(as.data.frame(raster::getValues(x))) 
+  names(dat) <- c('value', 'variable')
+  
+  dat <- dplyr::as.tbl(data.frame(coords, dat))
+  
+  if (!is.null(levels(x))) {
+    dat <- dplyr::left_join(dat, levels(x)[[1]], 
+                            by = c("value" = "ID"))
+  }
+  dat
+}
+
+# Turn deficit raster into table
+space.data <- gplot_data(space)
+
+## Plot into map
 p <- ggplot() +
-  # state outlines
-  geom_sf(data = NAmer, color = "#808B96", fill = "white") +
-  geom_sf(data = IntWsts, color = "#808B96", fill = "#EAECEE") +
+  geom_tile(data = space.data, aes(x = x, y = y, fill = value),
+            show.legend = FALSE) +
+  scale_fill_gradient("Probability of juvenile presence",
+                      low = palette[6], high = palette[1]) +
+  geom_sf(data = nonIntWest, color = "#808B96", fill = "white") +
+  geom_sf(data = IntWsts, color = "#808B96", fill = NA) +
   coord_sf(xlim = c(-121, -100), ylim = c(30, 50), expand = FALSE) +
-  geom_point(data = data.psme, aes(x = LON_FS, y = LAT_FS, col = node),
-             size = 5, alpha = 0.7) +
-  scale_color_manual(values = pal[loop.ready],
-                     labels = labels) + 
   theme_bw(base_size = 18) +
   theme(panel.grid.major = element_line(color = "#808B96"), # blend lat/long into background
         panel.background = element_rect(fill = "#808B96"),
         axis.title = element_blank(),
-        legend.title = element_blank(),
+        # legend.title = element_blank(),
         legend.background = element_rect(color = "#808B96"),
-        legend.justification=c(0,0), # defines which side of legend .position coords refer to
+        legend.justification=c(0,0), # defines which side oflegend .position coords refer to 
         legend.position=c(0,0),
         legend.text=element_text(size=12))
 p
-# tiff(paste0(out.dir,"psme_tree_map_",currentDate,".tiff"),
+# tiff(paste0(out.dir,"def_map_",currentDate,".tiff"),
 #    width = 450, height = 600, units = "px")
 # p
 # dev.off()
+
+
+
+
